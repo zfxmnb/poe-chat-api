19636c1cdab3a9e2df525ccf82e106d6127c3e0f
zengfanxing <zengfanxing@joyy.com>
2024-05-13 19:10:31 +0800

    feat: 优化登录逻辑

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import random
 import string
 import hashlib
 import re
+import json
+from poe_api_wrapper import PoeApi
 # 获取环境变量的值
 CACHE_DIR = os.environ.get('PEO_CACHE_DIR')
 if CACHE_DIR is None:
@@ -14,15 +16,14 @@ PORT = os.environ.get('PEO_PORT')
 if PORT is None:
     PORT = 5000
 COOKIE_KEY='t_token'
-# Using poe.com tokens
-tokens = {
-    'b': '',
-    'lat': '',
-    'pwd': '',
-    'sessionToken': '',
-}
 listens = {}
-
+# 基础方法
+## 生成随机字符串
+def gen_hash(s = ''):
+    letters = string.ascii_letters
+    k = f"{''.join(random.choice(letters) for _ in range(32))}_{s}"
+    return hashlib.md5(str(k).encode('utf-8')).hexdigest()
+## 文件变动监听进程
 def watch_fork(file_path, callback):
     if os.path.exists(file_path):
         listens[file_path] = os.path.getmtime(file_path)
@@ -37,12 +38,88 @@ def watch_fork(file_path, callback):
             callback()
             listens[file_path] = current_time
         time.sleep(15)
-
+## 文件变动监听
 def watch_file(file_path, callback): 
     p = Thread(target=watch_fork, args=(file_path, callback))
     p.setDaemon(True)
     p.start()
+# 用户相关方法
+users = []
+userMap = {}
+usersPath = f'{CACHE_DIR}/users.json'
+## 获取所有用户
+def getUsers():
+    global users
+    global userMap
+    users = json.load(open(usersPath,'r'))
+    for i in range(0, len(users)):
+        user = users[i]
+        userMap[user['username']] = i
+    print(f'getUsers success')
+    return users
+getUsers()
+## 获取指定用户
+def getUser(name):
+    if len(users) == 0:
+        getUsers()
+    if len(users) == 0:
+        return None
+    return users[userMap[name]]
+## 设置指定用户session
+def setUserSession(user, session):
+    user['session'] = session
+    with open(usersPath, "w") as file:
+        file.write(json.dumps(users))
+    return True
+## 移除指定用户session
+def removeUserSession(name):
+    user = users[userMap[name]]
+    if user is None:
+        return False
+    del user['session']
+    with open(usersPath, "w") as file:
+        file.write(json.dumps(users))
+    return True
+## 用户登录设置session
+def userSignIn (name, pwd):
+    user = getUser(name)
+    if user is None or pwd != user['password']:
+        return None
+    k = str(pwd).encode('utf-8')
+    d = hashlib.md5(k).hexdigest()
+    t = gen_hash(d)
+    user['session'] = t
+    setUserSession(user, t)
+    return t
+## 通过session匹配用户有效
+def matchUserBySession(session):
+    if session is None:
+        return None
+    for i in range(0, len(users)):
+        if users[i]['session'] is not None and users[i]['session'] == session:
+            return users[i]
+            break
+    return None
+## 通过session匹配用户有效
+def validUserBySession(session):
+    if matchUserBySession(session) is None:
+        return False
+    return True
+## 登出
+def userSignOut(session):
+    user = matchUserBySession(session)
+    if user is None:
+        return False
+    del user['session']
+    with open(usersPath, "w") as file:
+        file.write(json.dumps(users))
+    return True
 
+# token方法
+tokens = {
+    'b': '',
+    'lat': '',
+}
 b_path = f'{CACHE_DIR}/b'
 lat_path = f'{CACHE_DIR}/lat'
 def getTokens():
@@ -56,35 +133,8 @@ def getTokens():
         tokens['lat'] = lat
         print(f'getTokens success: {tokens}')
 getTokens()
-
-pwd_path = f'{CACHE_DIR}/pwd'
-def getPwd():
-    global tokens
-    if os.path.exists(pwd_path):
-        with open(pwd_path, 'r') as file:
-            tokens['pwd'] = file.read()
-        print(f'getPwd success: {tokens}')
-getPwd()
-
-if tokens['pwd'] != '':
-    k = str(tokens['pwd']).encode('utf-8')
-    sessionToken_path = f'{CACHE_DIR}/sessions/{hashlib.md5(k).hexdigest()}'
-def getSessionToken():
-    global tokens
-    global sessionToken_path
-    if os.path.exists(sessionToken_path) is False:
-        return
-    k = str(tokens['pwd']).encode('utf-8')
-    sessionToken_path = f'{CACHE_DIR}/sessions/{hashlib.md5(k).hexdigest()}'
-    if os.path.exists(sessionToken_path):
-        with open(sessionToken_path, 'r') as file:
-            tokens['sessionToken'] = file.read()
-        print(f'getSessionToken success: {tokens}')
-getSessionToken()
-
-html = {
-    'content': ''
-}
+# 获取入口html
+html = {'content': ''}
 html_path = 'static/index.html'
 def getHTML():
     global html
@@ -94,14 +144,14 @@ def getHTML():
         print('getHTML success')
 getHTML()
 
-from poe_api_wrapper import PoeApi
-cookie={'b': tokens['b'], 'lat': tokens['lat']}
-print(f'cookie:{cookie}')
-poeApi = { 'client': None }
+# peo
+print(f'tokens:{tokens}')
+poeApi = { 'client': None, 'chats': [], 'chatMap': {} }
 def clientInit():
+    global poeApi
     poeApi['client'] = PoeApi(cookie=tokens)
-
 def clientCheck():
+    global poeApi
     if poeApi['client'] is None:
         clientInit()
 def updateTokens():
@@ -109,22 +159,19 @@ def updateTokens():
     getTokens()
     if poeApi['client'] is not None:
         clientInit()
-
-chatsRef = { 'chats': [] }
-chatMap = {}
 # 获取聊天记录
 def getChats() :
     clientCheck()
     history = poeApi['client'].get_chat_history()['data']
-    chatsRef['chats'] = []
+    poeApi['chats'] = []
 
     for key, value in history.items():
         for i in range(0, len(value)):
             chat = value[i]
             chat['bot'] = key
-            chatsRef['chats'].append(chat)
-            chatMap[str(chat['chatId'])] = chat
-    return chatsRef['chats']
+            poeApi['chats'].append(chat)
+            poeApi['chatMap'][str(chat['chatId'])] = chat
+    return poeApi['chats']
 # 发送消息
 def sendMsg(bot, message, chatId):
     # print(bot, chatId, message)
@@ -134,6 +181,7 @@ def sendMsg(bot, message, chatId):
     s = chunk['text']
     return s
 
+# Flask
 app = Flask(__name__)
 app.config['STATIC_FOLDER'] = 'static'
 app.config['TEMPLATES_FOLDER'] = 'static'
@@ -141,94 +189,73 @@ app.config['JSON_AS_ASCII'] = False
 app.json.ensure_ascii = False
 app.config['PORT'] = PORT
 
-# 生成随机字符串
-def gen_hash(s = ''):
-    letters = string.ascii_letters
-    k = f"{''.join(random.choice(letters) for _ in range(32))}_{s}"
-    return hashlib.md5(str(k).encode('utf-8')).hexdigest()
-
+# 鉴权
 def auth():
     t = request.cookies.get(COOKIE_KEY)
-    if t is None:
-        return False
-    k = str(tokens['pwd']).encode('utf-8')
-    path = f'{CACHE_DIR}/sessions/{hashlib.md5(k).hexdigest()}'
-    print(t, k, path)
-    if os.path.exists(path) is False:
-        return False
-    with open(path, 'r') as file:
-        tokensText = file.read()
-    print(t, tokensText)
-    if tokensText is None or re.search(t, tokensText) is None:
+    if t is None and validUserBySession(t) is False:
         return False
     return True
-
+# 重定向登录
 def loginRedirect():
-    resp = make_response(redirect('/login', code=302))
+    t = request.cookies.get(COOKIE_KEY)
+    if t is not None:
+        userSignOut(t)
+    resp = make_response(redirect('/signin', code=302))
     resp.delete_cookie(COOKIE_KEY)
     return resp
-
 # 发送聊天信息
 @app.route('/api/chats/<chatId>/<message>', methods=['GET'])
 def sendMessage(chatId, message):
     if auth() is False:
         return { 'status': False, 'code': -1, 'data': 'error' }
-    if chatMap.get(chatId) is None:
-        chatsRef['chats'] = getChats()
-    if chatMap.get(chatId) is None:
+    poeApi['chats'] = getChats()
+    if poeApi['chatMap'].get(chatId) is None:
+        poeApi['chats'] = getChats()
+    if poeApi['chatMap'].get(chatId) is None:
         return { 'status': False, 'code': 1, 'data': 'error' }
-    reply=sendMsg(chatMap.get(chatId)['bot'], message, chatId)
+    reply=sendMsg(poeApi['chatMap'].get(chatId)['bot'], message, chatId)
     return {'status': True, 'code': 0, 'data': { 'send': message, 'reply': reply }}
 # 获取单个信息
 @app.route('/api/chats/<chatId>', methods=['GET'])
 def queryChat(chatId):
     if auth() is False:
         return { 'status': False, 'code': -1, 'data': 'error' }
-    if chatMap.get(chatId) is None:
-        chatsRef['chats'] = getChats()
-    if chatMap.get(chatId) is None:
+    poeApi['chats'] = getChats()
+    if poeApi['chatMap'].get(chatId) is None:
+        poeApi['chats'] = getChats()
+    if poeApi['chatMap'].get(chatId) is None:
         return { 'status': False, 'code': 1, 'data': 'error' }
-    return {'status': True, 'code': 0, 'data': chatMap.get(chatId)}
+    return {'status': True, 'code': 0, 'data': poeApi['chatMap'].get(chatId)}
 # 获取所有聊天
 @app.route('/api/chats', methods=['GET'])
 def queryChats():
     if auth() is False:
         return { 'status': False, 'code': -1, 'data': 'error' }
-    chatsRef['chats'] = getChats()
-    if chatsRef['chats'] is None:
+    poeApi['chats'] = getChats()
+    if poeApi['chats'] is None:
         return { 'status': False, 'code': 1, 'data': 'error' }
-    return {'status': True, 'code': 0, 'data': chatsRef['chats']}
+    return {'status': True, 'code': 0, 'data': poeApi['chats']}
 # 登出接口
 @app.route('/api/signout', methods=['GET', 'POST'])
-def signout():
+def sign_out():
     resp = make_response({'status': True, 'code': -1, 'data': ''})
     resp.delete_cookie(COOKIE_KEY)
     return resp
 # 登录接口
 @app.route('/api/signin', methods=['POST'])
-def signin():
-    if tokens['pwd'] and tokens['pwd'] == request.json['password']:
-        k = str(request.json['password']).encode('utf-8')
-        d = hashlib.md5(k).hexdigest()
-        t = gen_hash(d)
-        path = f'{CACHE_DIR}/sessions/{d}'
-        if os.path.exists(path):
-            with open(path, "r+") as file:
-                file.write(f'{t}\n')
-        else:
-            with open(path, "a") as file:
-                file.write(t)
-                file.close()
+def sign_in():
+    t = userSignIn(request.json['username'], request.json['password'])
+    if t is not None:
         resp = make_response({ 'status': True, 'code': 0, 'data': '' })
         resp.set_cookie(COOKIE_KEY, t)
         return resp
     return {'status': False, 'code': 1, 'data': 'password invalid'}
-# 登出
-@app.route('/logout', methods=['GET', 'POST'])
+# 登出页
+@app.route('/signout', methods=['GET'])
 def logout():
     return loginRedirect()
 # 登录页
-@app.route('/login', methods=['GET'])
+@app.route('/signin', methods=['GET'])
 def login():
     if auth() is True:
         return redirect(url_for('index'), code=302)
@@ -239,19 +266,16 @@ def login():
 def index():
     if auth() is False:
         return loginRedirect()
-    return render_template('index.html', data={ 'page': 'login', 'chats': chatsRef['chats'] }, content=html['content'])
+    return render_template('index.html', data={ 'page': 'login', 'chats': poeApi['chats'] }, content=html['content'])
 # 定义当出现 404 错误时的处理函数
 @app.errorhandler(404)
 def page_not_found(error):
-    if auth() is False:
-        return
     return redirect(url_for('index'))
 
 def main ():
+    watch_file(usersPath, getUsers)
     watch_file(b_path, updateTokens)
     watch_file(lat_path, updateTokens)
-    watch_file(pwd_path, getPwd)
-    watch_file(sessionToken_path, getSessionToken)
     watch_file(html_path, getHTML)
     app.run(port=PORT)
 if __name__ == '__main__':
